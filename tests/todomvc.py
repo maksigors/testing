2ab55b30aa999ceb0b0320f9b2763217d25a0d9d
maksigors <maksigors@gmail.com>
2022-02-10 16:54:25 +0200

    add all files

--- tests/todomvc.py
+++ tests/todomvc.py
@@ -26,15 +26,17 @@ def test_add_tasks_xpath():
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
 
+    # browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
+    #                 '//*[@class="toggle"]').click()
     browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
-                    '//*[@class="toggle"]').click()
+                    '//*[contains(concat(" ", normalize-space(@class), " "), " toggle ")]') \
+        .click()
 
     browser.all('#todo-list li:not(.completed)').should(have.exact_texts('a','c'))
     browser.all('#todo-list li.completed').should(have.exact_text('b'))
 
     browser.element('//*[@id="filters"]//a[.//text()="Completed"]').click()
     browser.element('//*[@id="filters"]//a[.//text()="Active"]').click()
-
 #end__DEF#
 
 #   =====================================
