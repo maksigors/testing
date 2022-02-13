1bee7d01cf4f76212d798c6ff32e2465bfc7cde9
maksigors <maksigors@gmail.com>
2022-02-13 14:53:32 +0200

    v105

--- tests/ts_todomvc/test_complete_css.py
+++ tests/ts_todomvc/test_complete_css.py
@@ -1,23 +1,6 @@
 from selene import by, be, have
 from selene.support.shared import browser, config
 
-def test_todo_complete_selene():
-    browser.open('https://todomvc.com/examples/emberjs/')
-    browser.element('#new-todo').type('a').press_enter()
-    browser.element('#new-todo').type('b').press_enter()
-    browser.element('#new-todo').type('c').press_enter()
-
-    browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
-
-    browser.all('#todo-list>li').element_by(have.exact_text('b')) \
-        .element('.toggle').click()
-
-    browser.all('#todo-list>li').filtered_by(have.css_class('completed')) \
-        .should(have.exact_texts('b'))
-    browser.all('#todo-list>li').filtered_by(have.no.css_class('completed')) \
-        .should(have.exact_texts('a', 'c'))
-    print('hello')
-
 def test_todo_complete_css():
     browser.open('https://todomvc.com/examples/emberjs/')
     browser.element('#new-todo').type('a').press_enter()
@@ -26,8 +9,8 @@ def test_todo_complete_css():
 
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
-    browser.element('#todo-list>li:nth-of-type(2)') \
-        .element('.toggle').click()
+    browser.element('#todo-list>li:nth-of-type(2) .toggle') \
+        .click()
     browser.element('#todo-list>li.completed') \
         .should(have.exact_text('b'))
     browser.all('#todo-list>li:not(.completed)') \
