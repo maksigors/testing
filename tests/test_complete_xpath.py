70fdc04d71b1c3c9edd3835739da7a60e25c722e
maksigors <maksigors@gmail.com>
2022-02-16 14:34:06 +0200

    v110

--- tests/test_complete_xpath.py
+++ tests/test_complete_xpath.py
@@ -7,8 +7,7 @@ def test_add_tasks_xpath():
     browser.element('//*[@id="new-todo"]').type('b').press_enter()
     browser.element('//*[@id="new-todo"]').type('c').press_enter()
 
-    # browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
-    #                 '//*[@class="toggle"]').click()
+    # browser.element('//*[@id="todo-list"]//li[.//text()="b"]//*[@class="toggle"]').click()
     browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
                     '//*[contains(concat(" ", normalize-space(@class), " "), " toggle ")]') \
         .click()
@@ -18,7 +17,6 @@ def test_add_tasks_xpath():
     browser.all('//*[@id="new-todo"]//*[not(contains(@class, "toggle"))]') \
         .should(have.exact_text('c'))
     browser.all('//*[@id="new-todo"]//li[@class="toggle"]').should(have.exact_text('b'))
-    #browser.all('#todo-list li:not(.completed)').should(have.exact_texts('a', 'c'))
 
     browser.element('//*[@id="filters"]//a[.//text()="Completed"]').click()
     browser.element('//*[@id="filters"]//a[.//text()="Active"]').click()
