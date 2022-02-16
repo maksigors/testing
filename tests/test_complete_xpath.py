2769a0c78507196f2ca2912332a6fced7c1bfe59
maksigors <maksigors@gmail.com>
2022-02-16 14:31:16 +0200

    v110

--- tests/test_complete_xpath.py
+++ tests/test_complete_xpath.py
@@ -3,9 +3,9 @@ from selene.support.shared import browser, config
 
 def test_add_tasks_xpath():
     browser.open('https://todomvc.com/examples/emberjs/')
-    browser.element('#new-todo').type('a').press_enter()
-    browser.element('#new-todo').type('b').press_enter()
-    browser.element('#new-todo').type('c').press_enter()
+    browser.element('//*[@id="new-todo"]').type('a').press_enter()
+    browser.element('//*[@id="new-todo"]').type('b').press_enter()
+    browser.element('//*[@id="new-todo"]').type('c').press_enter()
 
     # browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
     #                 '//*[@class="toggle"]').click()
@@ -13,8 +13,12 @@ def test_add_tasks_xpath():
                     '//*[contains(concat(" ", normalize-space(@class), " "), " toggle ")]') \
         .click()
 
-    browser.all('#todo-list li:not(.completed)').should(have.exact_texts('a','c'))
-    browser.all('#todo-list li.completed').should(have.exact_text('b'))
+    browser.all('//*[@id="new-todo"]//*[not(contains(@class, "toggle"))]') \
+        .should(have.exact_text('a'))
+    browser.all('//*[@id="new-todo"]//*[not(contains(@class, "toggle"))]') \
+        .should(have.exact_text('c'))
+    browser.all('//*[@id="new-todo"]//li[@class="toggle"]').should(have.exact_text('b'))
+    #browser.all('#todo-list li:not(.completed)').should(have.exact_texts('a', 'c'))
 
     browser.element('//*[@id="filters"]//a[.//text()="Completed"]').click()
     browser.element('//*[@id="filters"]//a[.//text()="Active"]').click()
