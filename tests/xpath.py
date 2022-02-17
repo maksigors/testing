8421e1b5c434d7ee6e38b912aa394109e06549cc
maksigors <maksigors@gmail.com>
2022-02-17 21:08:54 +0200

    v331

--- tests/xpath.py
+++ tests/xpath.py
@@ -15,9 +15,8 @@ browser.element('//*[@id="new-todo"]').type('a').press_enter()
 browser.element('//*[@id="new-todo"]').type('b').press_enter()
 browser.element('//*[@id="new-todo"]').type('c').press_enter()
 
-browser.element('//*[@id="todo-list "]//li[.//text()="b"]//*' + filter_by_class('toggle')) \
-    .click()
-
+browser.element('//*[@id="todo-list"]//li[.//text()="b"]'
+                '//*' + filter_by_class('toggle')).click()
 
 browser.all('//*[@id="new-todo"]//*' + by_no_class("toggle")).should(have.exact_text('a'))
 browser.all('//*[@id="new-todo"]//*' + by_no_class("toggle")).should(have.exact_text('c'))
