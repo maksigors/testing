2d4871ff456fbdb001133fb0c00e5b1d0a15ae9c
maksigors <maksigors@gmail.com>
2022-02-17 19:08:30 +0200

    v300

--- tests/xpath_filter.py
+++ tests/xpath_filter.py
@@ -2,7 +2,7 @@ from selene import by, be, have
 from selene.support.shared import browser, config
 
 def by_no_class(css_class : str) -> str:
-    return f'[not(contains(@class, " {css_class} "))]'
+    return f'[not(contains(@class, "{css_class}"))]'
 
 
 browser.open('https://todomvc.com/examples/emberjs/')
@@ -10,4 +10,4 @@ browser.element('//*[@id="new-todo"]').type('a').press_enter()
 browser.element('//*[@id="new-todo"]').type('b').press_enter()
 browser.element('//*[@id="new-todo"]').type('c').press_enter()
 
-browser.all('//*[@id="todo-list"]//*' + by_no_class('toggle')).should(have.exact_text('c'))
+browser.all('//*[@id="new-todo"]//*' + by_no_class("toggle")).should(have.exact_text('c'))
